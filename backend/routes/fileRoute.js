24149973d89e3088d0414f58c8f9e889a8e66fc6
dhruv <dhruv.lotia@gmail.com>
2024-11-23 16:08:00 +0530

    prettier

--- backend/routes/fileRoute.js
+++ backend/routes/fileRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const fileController = require("../controllers/fileController");
 
-// Routes
+
 router.post("/upload", fileController.uploadFile);
 router.get("/convert/:fileId", fileController.convertFile);
 
